60c460aa66115334c57a99a39096256ac8de55db
Patrick Quist <partouf@gmail.com>
2023-03-03 22:32:34 +0100

    Update buildall.sh

--- buildall.sh
+++ buildall.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 find . -type f -print0 | xargs -0 dos2unix
-cd libGroundfloor && sh build.sh && cd ../libJumpropes && sh build.sh && cd ../libJumpknots && sh build.sh && cd ../libMySQLBooks && sh build.sh && cd ../libChalkboard && sh build.sh
+cd Groundfloor && sh build.sh && cd ../Jumpropes && sh build.sh
+# && cd ../Jumpknots && sh build.sh && cd ../MySQLBooks && sh build.sh && cd ../Chalkboard && sh build.sh
